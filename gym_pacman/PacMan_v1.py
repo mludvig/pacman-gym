570bf82b90d25039d13aa59d2358e0c5e4d82483
Michael Ludvig <mludvig@logix.net.nz>
2019-12-10 17:23:21 +1300

    Added basic rendering

--- gym_pacman/PacMan_v1.py
+++ gym_pacman/PacMan_v1.py
@@ -52,6 +52,11 @@ class PacMan_v1(gym.Env):
     - visiting a new cell is -1 + 1 = 0
     """
 
+    metadata = {
+        'render.modes': ['human', 'rgb_array'],
+        'video.frames_per_second': 2
+    }
+
     _repeat_multiplier_ = 4     # prevent "kernel is bigger than input" error
 
     def __init__(self, board_size=(5,5), max_moves=100):
@@ -96,6 +101,9 @@ class PacMan_v1(gym.Env):
         # Episode counter
         self._episode = 0
 
+        #  Rendering viewer
+        self._viewer = None
+
         self.reset()
 
     def reset(self):
@@ -122,6 +130,9 @@ class PacMan_v1(gym.Env):
 
         # Episode is not yet over
         self.is_over = False
+
+        # Reset rendering viewer
+        self._viewer = None
 
         # Count some stats
         self._episode += 1
@@ -196,8 +207,51 @@ class PacMan_v1(gym.Env):
             self._layer_1_pacman,
         ], axis=2).repeat(axis=0, repeats=self._repeat_multiplier_).repeat(axis=1, repeats=self._repeat_multiplier_)  # scale up the array to prevent "kernel is bigger than input" error
 
+
     def render(self, mode='human'):
-        return
+        def _idx2geom(np_xy):
+            # Calculate viewer dot center from numpy array index
+            pix_x = np_xy[0] * cell_size + cell_size/2
+            pix_y = np_xy[1] * cell_size + cell_size/2
+            return (pix_x, pix_y)
+
+        def _build_board():
+            board = []
+            for x in range(self._board_size[0]):
+                board.append([])
+                for y in range(self._board_size[1]):
+                    dot = rendering.make_circle(cell_size * 0.5/2)
+                    dot_pos = _idx2geom((x, y))
+                    dot.add_attr(rendering.Transform(translation=dot_pos))
+                    dot.set_color(0, 0, 0)
+                    self._viewer.add_geom(dot)
+                    board[x].append(dot)
+            return board
+
+        cell_size = 20
+        screen_width = self._board_size[0] * cell_size
+        screen_height = self._board_size[1] * cell_size
+
+        if self._viewer is None:
+            from gym.envs.classic_control import rendering
+            self._viewer = rendering.Viewer(screen_width, screen_height)
+
+            self._board_dots = _build_board()
+            self._pacmantrans = rendering.Transform()
+            self._pacman = rendering.make_circle(cell_size*0.8/2, filled=True)
+            self._pacman.set_color(0.8, 0.8, 0)
+            self._pacman.add_attr(self._pacmantrans)
+            self._viewer.add_geom(self._pacman)
+
+        # Clear dot under our position
+        self._board_dots[self.position[0]][self.position[1]].set_color(0.9, 0.9, 0.9)
+
+        # Move PacMan
+        new_x, new_y = _idx2geom(self.position)
+        self._pacmantrans.set_translation(new_x, new_y)
+
+        return self._viewer.render(return_rgb_array = mode=='rgb_array')
+
 
     def seed(self, seed=None):
         """
@@ -209,3 +263,9 @@ class PacMan_v1(gym.Env):
         """
         random.seed(seed)
         np.random.seed(seed)
+
+
+    def close(self):
+        if self._viewer:
+            self._viewer.close()
+            self._viewer = None
